8de3ff5086fa216de89cf66cc476c9e44273cf28
Jhonathan <nathan3boss@gmail.com>
2024-09-21 00:03:15 -0300

    fix: adjust size imgs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import { GeistSans } from 'geist/font/sans';
+import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "TEarth",
+  title: 'Earth',
   description: "DSCOVR's EPIC",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>{children}</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
-import axios from "axios";
-import type { NextPage } from "next";
-import { format } from "date-fns";
-import Image from "next/image";
+import axios from 'axios';
+import type { NextPage } from 'next';
+import { format } from 'date-fns';
+import Image from 'next/image';
 
-import { env } from "~/env";
-import BlurFade from "~/components/magicui/blur-fade";
-import { ScrollArea } from "~/components/ui/scroll-area";
-import Particles from "~/components/magicui/particles";
+import { env } from '~/env';
+import BlurFade from '~/components/magicui/blur-fade';
+import { ScrollArea } from '~/components/ui/scroll-area';
+import Particles from '~/components/magicui/particles';
 
 type NaturalItem = {
   image: string;
   date: string;
 };
 
+export const revalidate = 13;
+
+const baseAssetsDomain = 'https://api.nasa.gov/EPIC/archive/natural';
+const baseApiDomain = "https://api.nasa.gov/EPIC/api/natural"
+
 const Home: NextPage = async () => {
-  const { data, status } = await axios.get<NaturalItem[]>(
-    "https://api.nasa.gov/EPIC/api/natural?api_key=".concat(env.NASA_API_KEY),
-  );
+  const { data, status } = await axios.get<NaturalItem[]>(baseApiDomain + '?api_key='.concat(env.NASA_API_KEY));
 
   if (status == 200) {
     const items = data.map((e) => {
       const date = new Date(e.date);
-      const src = `https://api.nasa.gov/EPIC/archive/natural/${format(date, "yyyy")}/${format(date, "MM")}/${format(date, "dd")}/png/${e.image}.png?api_key=${env.NASA_API_KEY}`;
+
+      const src = `${baseAssetsDomain}/${format(date, 'yyyy')}/${format(date, 'MM')}/${format(date, 'dd')}/png/${e.image}.png?api_key=${env.NASA_API_KEY}`;
 
       return {
         date,
@@ -30,23 +34,23 @@ const Home: NextPage = async () => {
     });
 
     return (
-      <ScrollArea className="bg-ctp-crust relative h-screen w-screen overflow-y-auto">
+      <ScrollArea className="relative h-screen w-screen overflow-y-auto bg-ctp-crust">
         <Particles
-          className="fixed z-0 w-full h-full pointer-events-none"
+          className="pointer-events-none fixed z-0 h-full w-full"
           quantity={100}
           ease={80}
-          color={"#ffffff"}
+          color={'#ffffff'}
           refresh
         />
 
-        <div className="w-full z-10 p-10">
+        <div className="z-10 w-full p-10">
           <div className="mx-auto flex w-full flex-wrap justify-center gap-4">
             {items.map((e, idx) => (
               <BlurFade key={e.src} delay={0.25 + idx * 0.05} inView>
                 <Image
                   width={400}
                   height={400}
-                  className="size-full max-w-[300px] rounded-3xl object-contain"
+                  className="size-full max-w-[600px] rounded-3xl object-contain"
                   src={e.src}
                   alt={e.src}
                 />
@@ -58,11 +62,7 @@ const Home: NextPage = async () => {
     );
   }
 
-  return (
-    <div>
-      <h1>Error</h1>
-    </div>
-  );
+  return <div className="h-full w-full" />;
 };
 
 export default Home;
